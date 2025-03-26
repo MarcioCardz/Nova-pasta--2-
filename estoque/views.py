3a9b6c39129e0ccfa7a7d95129050f8602178714
Marcio <marcinho.lc@hotmail.com>
2025-03-26 20:44:45 -0300

    Estoque html produto

--- estoque/views.py
+++ estoque/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from estoque.models import Categoria
+
+
+def add_produto(request):
+   if request.method == 'GET':
+      categorias = Categoria.objects.all()
+      return render(request, 'add_produto.html', {'categorias': categorias} )
